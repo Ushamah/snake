6a81b52ba580bb2d81608ae115d591d194d448ed
Ushamah Wamala <extern.wamala_ushamah@allianz.com>
2021-08-25 16:04:30 +0200

    initial commit

---
+++ src/SnakeFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class SnakeFrame extends JFrame {
+
+     public SnakeFrame() {
+
+        this.add(new SnakePanel());
+        this.setTitle("SNAKE");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
